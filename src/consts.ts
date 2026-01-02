aaf1108ca9205a284b1eba359165a9d66b5fc43f
Nick Dichev <github@dichev.email>
2026-01-02 12:51:42 -0800

    Update site title and twitter handle

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const SITE_URL = "https://nickdichev.com";
 export const SITE_NAME = "Relax, it's software.";
-export const SITE_TITLE = "Relax, it's software | A personal blog by Nick Dichev";
-export const SITE_DESCRIPTION = "Relax, it's software | A personal blog by Nick Dichev";
-export const TWITTER_HANDLE = "https://twitter.com/nickdichev";
+export const SITE_TITLE = "Relax, it's software";
+export const SITE_DESCRIPTION = "Relax, it's software | A blog by Nick Dichev";
+export const TWITTER_HANDLE = "https://twitter.com/nicckk";
 export const GITHUB_HANDLE = "https://github.com/nickdichev";
 export const CLOUDINARY_NAME = "example";
