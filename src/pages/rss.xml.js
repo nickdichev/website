5ed5508a280531e77413cdb0cc065f13ae823e44
Nick Dichev <github@dichev.email>
2026-01-02 12:28:43 -0800

    Don't show planned posts in the rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,10 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
-export async function get(context) {
-  const posts = await getCollection("blog");
+export async function GET(context) {
+  const posts = await getCollection("blog", ({ data }) => {
+    return data.draft !== true && data.planned !== true;
+  });
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
